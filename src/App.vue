7f4028c9684836b793dd9b54b66f5b9326010ba3
Vadim Cpp <vadim.cpp@gmail.com>
2017-12-07 23:08:42 +0200

    Added home screen

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
     <header>
-      <span>Vue.js PWA</span>
+      <span>Тут будет шапка</span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
+    <footer>
+      <span>Тут будет завод</span>
+    </footer>
   </div>
+
 </template>
 
 <script>
@@ -26,6 +29,12 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 
 main {
@@ -41,6 +50,19 @@ header {
   color: #ffffff;
 }
 
+footer {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+
 header span {
   display: block;
   position: relative;
@@ -51,4 +73,16 @@ header span {
   box-sizing: border-box;
   padding-top: 16px;
 }
+
+footer span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <p>Тут будет навигация breadcrumbs</p>
+    <ul>
+      <li><router-link to="bus">1</router-link></li>
+      <li><router-link to="taxi">2</router-link></li>
+      <li><router-link to="administrator">3</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
