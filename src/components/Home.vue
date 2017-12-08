0d4b9fcb18795be21813dd90b60d375eac0b2e11
Vadim Cpp <vadim.cpp@gmail.com>
2017-12-09 01:10:52 +0200

    Добввил линки

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,16 +2,36 @@
   <div class="home">
     <p>Тут будет навигация breadcrumbs</p>
     <ul>
-      <li><router-link to="bus">1</router-link></li>
-      <li><router-link to="taxi">2</router-link></li>
-      <li><router-link to="administrator">3</router-link></li>
+      <li v-for="page in pages">
+        <router-link :to=page.route> {{ page.name }} </router-link>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      pages: [
+        { route: '1', name: 'Автобус Гусев-Калининград' },
+        { route: '2', name: 'Автобус Калининград-Гусев ' },
+        { route: '3', name: 'Автобус Гусев-Поселок' },
+        { route: '4', name: 'Автобус Поселок-Гусев' },
+        { route: '5', name: 'Администратор поселка' },
+        { route: '6', name: 'Заказ воды «Айсберг»' },
+        { route: '7', name: 'Кино «Люмен Фильм»' },
+        { route: '8', name: 'Карта поселка' },
+        { route: '9', name: 'Такси в Гусеве' },
+        { route: '10', name: 'Такси в Калининграде' },
+        { route: '11', name: 'Перевозка ваших вещей' },
+        { route: '12', name: 'ФОК города Гусева' },
+        { route: '13', name: 'Кафе #КЕБАБ' },
+        { route: '14', name: 'Кафе «Фьюжн экспресс»' },
+        { route: '15', name: 'О нас' }
+      ]
+    }
+  }
 }
 </script>
 
