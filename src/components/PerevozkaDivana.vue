0f039b85fc381a1d83b31fd6fb71fe2f789e49d4
Vadim Cpp <vadim.cpp@gmail.com>
2017-12-13 03:18:24 +0200

    Add perevozka divana

---
+++ src/components/PerevozkaDivana.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="perevozka-divana">
+
+    <!-- Breadcrumb -->
+    <section class="gs-container gs-container_center">
+      <ol class="gs-breadcrumb">
+        <li class="gs-breadcrumb__list-item"> <router-link class="gs-breadcrumb__link" :to="{ name: 'Home' }"> GS Гусев </router-link> </li>
+          <li class="gs-breadcrumb__list-item gs-breadcrumb__list-item_active">
+              Перевозка<span class="gs-breadcrumb__hidden-small-text"> ваших</span> вещей
+          </li>
+      </ol>
+    </section>
+
+    <!-- Thumbnail -->
+    <section class="gs-container gs-container_center gs-container_padding-bottom">
+      <div class="gs-thumbnail">
+        <img class="gs-thumbnail__image" src="img/ford_transit.jpg" alt="Ford Transit">
+        <div class="gs-thumbnail__caption gs-thumbnail__caption_text-center">
+            <p>
+                Вместительный Ford Transit безопасно доставит ваш груз в любую точку области.
+            </p>
+            <h4>Дмитрий</h4>
+            <p>8 (921) 617-60-13</p>
+            <a href="tel:89216176013" class="gs-call-button">
+                <i class="gs-icon-phone"> </i>
+                Позвонить
+            </a>
+        </div>
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PerevozkaDivana',
+  props: ['title'],
+  created () {
+    document.title = this.title
+  },
+  watch: {
+    title () {
+      // only used when the title changes after page load
+      document.title = this.title
+    }
+  },
+  render () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import '../assets/scss/perevozka-divana.scss';
+
+</style>
