03b2854140faa69218ebc526b5149cde603f0696
Vadim Cpp <vadim.cpp@gmail.com>
2017-12-13 03:03:28 +0200

    Update home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,39 +22,63 @@
     <!-- Main menu -->
     <section class="gs-container gs-container_center gs-container_padding-bottom">
         <div class="gs-main-menu gusev" id="main-menu">
-            <router-link :to="{ name: 'BusGusevKgd' }" class="gs-main-menu__link"> 
+            <router-link :to="{ name: 'BusGusevKgd' }" class="gs-main-menu__link" data-city="gusev kaliningrad"> 
                 Автобус Гусев-Калининград 
                 <i class="gs-icon-angle-right"> </i> 
             </router-link>
-            <router-link :to="{ name: 'BusKgdGusev' }" class="gs-main-menu__link"> 
+            <router-link :to="{ name: 'BusKgdGusev' }" class="gs-main-menu__link" data-city="gusev kaliningrad"> 
                 Автобус Калининград-Гусев 
                 <i class="gs-icon-angle-right"> </i> 
             </router-link>
-            <router-link :to="{ name: 'BusGusevPoselok' }" class="gs-main-menu__link"> 
+            <router-link :to="{ name: 'BusGusevPoselok' }" class="gs-main-menu__link" data-city="gusev"> 
                 Автобус Гусев-Поселок 
                 <i class="gs-icon-angle-right"> </i> 
             </router-link>
-            <router-link :to="{ name: 'BusPoselokGusev' }" class="gs-main-menu__link"> 
+            <router-link :to="{ name: 'BusPoselokGusev' }" class="gs-main-menu__link" data-city="gusev"> 
                 Автобус Поселок-Гусев 
                 <i class="gs-icon-angle-right"> </i> 
             </router-link>
-            <router-link :to="{ name: 'AdminPoselka' }" class="gs-main-menu__link"> 
+            <router-link :to="{ name: 'AdminPoselka' }" class="gs-main-menu__link" data-city="gusev"> 
                 Администратор поселка 
                 <i class="gs-icon-angle-right"> </i> 
             </router-link>
-            <router-link :to="{ name: 'Voda' }" class="gs-main-menu__link"> 
+            <router-link :to="{ name: 'Voda' }" class="gs-main-menu__link" data-city="gusev"> 
                 Заказ воды «Айсберг» 
                 <i class="gs-icon-angle-right"> </i> 
             </router-link>
-            <a href="kino_lumen.html" class="gs-main-menu__link" data-city="gusev"> Кино &laquo;Люмен Фильм&raquo; <i class="gs-icon-angle-right"> </i> </a>
-            <a href="karta_poselka.html" class="gs-main-menu__link" data-city="gusev"> Карта поселка <i class="gs-icon-angle-right"> </i> </a>
-            <a href="taxi_v_guseve.html" class="gs-main-menu__link" data-city="gusev"> Такси в Гусеве <i class="gs-icon-angle-right"> </i> </a>
-            <a href="taxi_v_kaliningrade.html" class="gs-main-menu__link" data-city="kaliningrad"> Такси в Калининграде <i class="gs-icon-angle-right"> </i> </a>
-            <a href="perevozka_divana.html" class="gs-main-menu__link" data-city="gusev"> Перевозка ваших вещей <i class="gs-icon-angle-right"> </i> </a>
-            <a href="gusev_fok.html" class="gs-main-menu__link" data-city="gusev"> ФОК города Гусева <i class="gs-icon-angle-right"> </i> </a>
-            <a href="kebab.html" class="gs-main-menu__link" data-city="gusev"> Кафе #КЕБАБ <i class="gs-icon-angle-right"> </i> </a>
-            <a href="fusion_express.html" class="gs-main-menu__link" data-city="kaliningrad"> Кафе &laquo;Фьюжн экспресс&raquo; <i class="gs-icon-angle-right"> </i> </a>
-            <router-link :to="{ name: 'About' }" class="gs-main-menu__link"> 
+            <router-link :to="{ name: 'KinoLumen' }" class="gs-main-menu__link" data-city="gusev"> 
+                Кино «Люмен Фильм» 
+                <i class="gs-icon-angle-right"> </i> 
+            </router-link>
+            <router-link :to="{ name: 'KartaPoselka' }" class="gs-main-menu__link" data-city="gusev"> 
+                Карта поселка 
+                <i class="gs-icon-angle-right"> </i> 
+            </router-link>
+            <router-link :to="{ name: 'TaxiVGuseve' }" class="gs-main-menu__link" data-city="gusev"> 
+                Такси в Гусеве 
+                <i class="gs-icon-angle-right"> </i> 
+            </router-link>
+            <router-link :to="{ name: 'TaxiVKaliningrade' }" class="gs-main-menu__link" data-city="kaliningrad"> 
+                Такси в Калининграде 
+                <i class="gs-icon-angle-right"> </i> 
+            </router-link>
+            <router-link :to="{ name: 'PerevozkaDivana' }" class="gs-main-menu__link" data-city="gusev"> 
+                Перевозка ваших вещей
+                <i class="gs-icon-angle-right"> </i> 
+            </router-link>
+            <router-link :to="{ name: 'GusevFok' }" class="gs-main-menu__link" data-city="gusev"> 
+                ФОК города Гусева
+                <i class="gs-icon-angle-right"> </i> 
+            </router-link>
+            <router-link :to="{ name: 'Kebab' }" class="gs-main-menu__link" data-city="gusev"> 
+                Кафе #КЕБАБ
+                <i class="gs-icon-angle-right"> </i> 
+            </router-link>
+            <router-link :to="{ name: 'FusionExpress' }" class="gs-main-menu__link" data-city="gusev"> 
+                Кафе «Фьюжн экспресс»
+                <i class="gs-icon-angle-right"> </i> 
+            </router-link>
+            <router-link :to="{ name: 'About' }" class="gs-main-menu__link" data-city="gusev kaliningrad"> 
                 О нас 
                 <i class="gs-icon-angle-right"> </i> 
             </router-link>
